fa8d3adb56fef5f4800e668070f6266519ffebfa
Bhagyesh Dere <derebhagyesh@gmail.com>
2026-03-17 01:50:40 +0530

    Updated homepage section flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Hero from "@/components/home/Hero"
 import About from "@/components/home/About"
-import Mission from "@/components/home/Mission"
 import WhyChoose from "@/components/home/WhyChoose"
 import Services from "@/components/home/Services"
-import Facilities from "@/components/home/Facilities"
 import Products from "@/components/home/Products"
+import Facilities from "@/components/home/Facilities"
+import Mission from "@/components/home/Mission"
 import Clients from "@/components/home/Clients"
 
 export default function Home() {
@@ -13,11 +13,11 @@ export default function Home() {
 
       <Hero />
       <About />
-      <Mission />
       <WhyChoose />
       <Services />
-      <Facilities />
       <Products />
+      <Facilities />
+      <Mission />
       <Clients />
 
     </main>
